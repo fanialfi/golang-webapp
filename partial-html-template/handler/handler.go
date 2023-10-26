d91f1efa9ca92fa67ef77929d4607f71603b6d16
fanialfi <fanialfirdaus@gmail.com>
2023-10-26 18:13:44 +0700

    render banyak partial html template menggunakan `template.ParseFiles`

--- partial-html-template/handler/handler.go
+++ partial-html-template/handler/handler.go
@@ -1,1 +1,39 @@
 package handler
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type M map[string]any
+
+func HandleIndex(res http.ResponseWriter, req *http.Request) {
+	data := M{"nama": "Batman"}
+
+	// template.Must digunakan untuk deteksi error pada saat pembuatan instance *template.Template
+	// ketika ada error, panic akan dimunculkan
+	tmpl := template.Must(template.ParseFiles(
+		"views/index.html",
+		"views/_header.html",
+		"views/_message.html",
+	))
+
+	err := tmpl.ExecuteTemplate(res, "index", data)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func HandleAbout(res http.ResponseWriter, req *http.Request) {
+	data := M{"nama": "Batman"}
+	tmpl := template.Must(template.ParseFiles(
+		"views/about.html",
+		"views/_header.html",
+		"views/_message.html",
+	))
+
+	err := tmpl.ExecuteTemplate(res, "about", data)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+	}
+}
